92a5abab71d732927e080839662f76300ed9d4d1
emilpoppler134 <emil@50archive.com>
2024-03-21 12:09:35 +0100

    Fix error alert style bugs

--- src/components/ErrorAlert.tsx
+++ src/components/ErrorAlert.tsx
@@ -14,11 +14,11 @@ const ErrorAlert: React.FC<Props> = ({ message, onClose }) => {
 
   return (
     <div className="error-alert">
-      <div className="flex items-center bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+      <div className="flex items-center bg-red-100 border border-red-400 text-red-700 pl-4 pr-16 py-3 rounded relative" role="alert">
         <ExclamationTriangleIcon className="mr-2 w-5 h-5 fill-red-500" />
         <span className="block sm:inline">{message}</span>
         <button
-          className="absolute top-0 bottom-0 right-0 px-4 py-3"
+          className="absolute top-0 bottom-0 right-0 px-4 py-2"
           onClick={handleButtonClick}
         >
           <XMarkIcon className="h-6 w-6 fill-red-500" />
