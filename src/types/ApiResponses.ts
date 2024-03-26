07f1620847b0181a5347f6bf8828397b7faf7197
emilpoppler134 <emil@50archive.com>
2024-03-26 14:16:17 +0100

    Add a util method that can be used to call the api in all scenarios

--- src/types/ApiResponses.ts
+++ src/types/ApiResponses.ts
@@ -4,16 +4,29 @@ export enum ResponseStatus {
 }
 
 export enum ErrorType {
+  UNAUTHORIZED = "UNAUTHORIZED",
+  TOKEN_ERROR = "TOKEN_ERROR",
   INVALID_PARAMS = "INVALID_PARAMS",
   DATABASE_ERROR = "DATABASE_ERROR",
   HASH_PARSING = "HASH_PARSING",
   USER_EXISTS = "USER_EXISTS",
+  BOT_EXISTS = "BOT_EXISTS",
   NO_RESULT = "NO_RESULT",
   MAIL_ERROR = "MAIL_ERROR",
 }
 
-export type ApiResponse = {
-  status: ResponseStatus;
-  data?: any;
-  error?: ErrorType;
+export type ErrorResponse = {
+  status: ResponseStatus.ERROR;
+  error: ErrorType;
 }
+
+export type ValidResponse = {
+  status: ResponseStatus.OK;
+}
+
+export type ValidDataResponse = {
+  status: ResponseStatus.OK;
+  data: any;
+}
+
+export type ApiResponse = ErrorResponse | ValidResponse | ValidDataResponse;
---
+++ src/utils/apiService.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosResponse } from "axios";
+
+import { API_ADDRESS } from "../config";
+import { ApiResponse, ValidResponse, ValidDataResponse, ErrorResponse, ResponseStatus, ErrorType } from "../types/ApiResponses";
+
+type Body = { [key: string]: string | undefined };
+
+export async function callAPI<T>(url: string, body?: Body): Promise<ValidResponse | ValidDataResponse & { data: T } | ErrorResponse | null> {
+  try {
+    const axiosResponse: AxiosResponse = await axios.post(API_ADDRESS + url, body);
+    const response: ApiResponse = axiosResponse.data;
+
+    if (response === null) {
+      return null;
+    }
+
+    if (response.status === ResponseStatus.ERROR) {      
+      if (response.error === ErrorType.UNAUTHORIZED) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      }
+      return { status: ResponseStatus.ERROR, error: response.error } as ErrorResponse;
+    }
+
+    const isValidDataResponse = (
+      (r: ValidResponse | ValidDataResponse): r is ValidDataResponse => {
+        return (r as ValidDataResponse).data !== undefined;
+      }
+    )(response);
+
+    if (!isValidDataResponse) {
+      return { status: ResponseStatus.OK } as ValidResponse;
+    }
+
+    return { status: ResponseStatus.OK, data: response.data } as ValidDataResponse & { data: T };
+  } catch {
+    return null;
+  }
+}
