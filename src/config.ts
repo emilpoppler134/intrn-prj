5c31f1f38be23cf7052612a9526d5e1c771a6073
emilpoppler134 <emil@50archive.com>
2024-05-07 11:31:23 +0200

    Add env variables

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const APP_ADDRESS: string = "https://netlight-6e85680640b6.herokuapp.com";
-export const API_ADDRESS: string = "https://netlight-api-7d8cc46c53b9.herokuapp.com";
-export const STRIPE_PUBLIC_KEY: string = "pk_test_51Ot3XhIsOGY7616qBXi49eLZeyyoTGyPp7soKX3S4Vk8djxS4wNpq7dwNnaVd2IN64DKDj40XCf6igOqVjvFffSW00IRGIxXVs";
+export const APP_ADDRESS: string = process.env.REACT_APP_ADDRESS || "";
+export const API_ADDRESS: string = process.env.REACT_APP_API_ADDRESS || "";
+export const STRIPE_PUBLIC_KEY: string =
+  process.env.REACT_APP_STRIPE_PUBLIC_KEY || "";
